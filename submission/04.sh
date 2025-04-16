e00893b84a6e97522a8ceaa9bdc6911cc068ebca
b-l-u-e <winnie.gitau282@gmail.com>
2025-04-16 18:06:12 +0300

    implemented bitcoin script creation and transaction analysis

--- submission/04.sh
+++ submission/04.sh
@@ -1,2 +1,16 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+# Set the locktime (timestamp)
+locktime=1495584032
+
+# Convert locktime to little-endian hex
+locktime_le=$(printf '%08x' $locktime | tac -rs ..)
+
+# Create pubkey hash
+pubkeyhash=$(echo -n "$publicKey" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p)
+
+# Build the script
+script="04${locktime_le}b17576a914${pubkeyhash}88ac"
+
+echo "$script"
--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,19 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
-# publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+
+# Set the number of blocks
+blocks=150
+
+# Convert blocks to little-endian hex
+blocks_hex=$(printf '%04x' $blocks)
+
+# Convert blocks_hex to CSV format
+csv_hex=$(echo $blocks_hex | sed 's/../& /g' | awk '{for(i=NF;i>0;i--) printf $i}')
+
+# Create pubkey hash
+pubkeyhash=$(echo -n "$publicKey" | xxd -r -p | openssl dgst -sha256 -binary | openssl dgst -rmd160 -binary | xxd -p)
+
+# Build the final script
+final_script="02${csv_hex}b27576a914${pubkeyhash}88ac"
+
+echo "$final_script"
